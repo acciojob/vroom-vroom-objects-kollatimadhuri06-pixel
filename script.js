14e0548aebfabf7bf9d801d39a9db011671c0f0b
kollatimadhuri06-pixel <kollatimadhuri06@gmail.com>
2026-05-22 16:49:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 // Complete the js code
+
 function Car(make, model) {
+
 	this.make = make
 	this.model = model
 	
 }
+
+
+// Parent method
 Car.prototype.getMakeModel = function(){
+
 	return `${this.make} ${this.model}`
 	
 }
 
-function SportsCar(make, model, topSpeed) {
-	Car.call(this,make,model)
-	this.topspeed = topSpeed
-}
-SportsCar.prototype = Object.create(Car.prototype)
-Sportscar.prototype.constructor = Sportscar;
 
-SportsCar.prototype.getTopSpeed = function {
-	return topSpeed
+// Child constructor
+function SportsCar(make, model, topSpeed) {
+
+	Car.call(this, make, model)
+
+	this.topSpeed = topSpeed
 }
-const car = new Sportscar("Ferrari","Testarossa",200)
- console.log(car.getMakeModel())
- console.log(car.getTopSpeed())
+
+
+// Inheritance
+SportsCar.prototype = Object.create(Car.prototype)
+
+SportsCar.prototype.constructor = SportsCar;
+
+
+// Child method
+SportsCar.prototype.getTopSpeed = function(){
+
+	return this.topSpeed
+
+}
+
+
+// Create object
+const car = new SportsCar("Ferrari", "Testarossa", 200)
+
+console.log(car.getMakeModel())
+
+console.log(car.getTopSpeed())
+
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
